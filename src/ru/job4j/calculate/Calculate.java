1165da67f319f55e12a1299fd0aea4b272ef0941
frolicdima <ppdnk@mail.ru>
2018-08-02 12:56:38 +0300

    1.3. JavaDoc. Code convention [#4087]

--- src/ru/job4j/calculate/Calculate.java
+++ src/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,15 @@
+/**
+ * Calculate.
+ *
+ * @author Dmitrii Mitin (mailto:ppdnk@mail.ru)
+ */
 package ru.job4j.calculate;
 
 public class Calculate {
+	/**
+	 * Main.
+	 * @param args - args.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
