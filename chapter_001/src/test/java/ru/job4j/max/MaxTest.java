30116b78f6f54f400d7f6a23bf104c05160095f4
frolicdima <ppdnk@mail.ru>
2018-09-22 09:21:03 +0300

    4.3. Вычисление площади треугольника [#9461]

--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -16,4 +16,11 @@ public class MaxTest {
         int result = maxim.max(1, 2);
         assertThat(result, is(2));
     }
+
+    @Test
+    public void whenFirstGreaterSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(100, 2);
+        assertThat(result, is(100));
+    }
 }
