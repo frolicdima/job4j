49fe20ded219b2842897113b8e94072f4fb396d9
frolicdima <ppdnk@mail.ru>
2018-11-06 22:04:24 +0300

    1. Реализация профессий в коде [#6837]

---
+++ chapter_002/src/main/java/ru/job4j/inheritance/Profession.java
@@ -0,0 +1,9 @@
+package ru.job4j.inheritance;
+
+public class Profession {
+    public String name;
+    public String profession;
+    public String getName() {
+        return this.name;
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/inheritance/Teacher.java
@@ -0,0 +1,7 @@
+package ru.job4j.inheritance;
+
+public class Teacher {
+    public void teach(Student) {
+
+    }
+}
