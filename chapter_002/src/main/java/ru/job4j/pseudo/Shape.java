ba231b88510fd984eb46aaa879e35cdedac6dbfe
frolicdima <ppdnk@mail.ru>
2018-12-05 21:07:02 +0300

    4. Используя шаблон проектирования - стратегию[#786]

---
+++ chapter_002/src/main/java/ru/job4j/pseudo/Shape.java
@@ -0,0 +1,10 @@
+package ru.job4j.pseudo;
+
+/**
+ * @author Dmitrii Mitin (ppdnk@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public interface Shape {
+    String draw();
+}
---
+++ chapter_002/src/main/java/ru/job4j/pseudo/Square.java
@@ -0,0 +1,18 @@
+package ru.job4j.pseudo;
+
+/**
+ * @author Dmitrii Mitin (ppdnk@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Square implements Shape {
+    @Override
+    public String draw() {
+        StringBuilder pic = new StringBuilder();
+        pic.append("++++");
+        pic.append("+     +");
+        pic.append("+     +");
+        pic.append("++++");
+        return pic.toString();
+    }
+}
